292ee2efc3d6732279de96eabae6dc4bc6b6a686
rishi <rishi88221@gmail.com>
2023-04-21 22:37:29 +0530

    basic

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/internship2k23website/',
+  base: '/swechaintern2k23/',
   
   plugins: [react()],
 })
